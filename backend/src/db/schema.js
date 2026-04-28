c26a5826d4765db454bdebeac4c438a8a63b2d3f
Gustavo Santiago <gustavohenriquesantiago5@gmail.com>
2026-04-28 17:09:27 -0300

    Add .env configuration, update schema, and enhance favorites API endpoints
    - Created .gitignore to exclude sensitive files and node_modules
    - Added EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY to .env
    - Fixed import statement in db.js
    - Changed userID to userId in schema and updated related API logic
    - Implemented delete and get favorites API endpoints with userId and recipeId

--- backend/src/db/schema.js
+++ backend/src/db/schema.js
@@ -4,7 +4,7 @@ import { numeric } from 'drizzle-orm/sqlite-core';
 
 export const favoritesTable = pgTable("favorites", {
     id: serial("id").primaryKey(),
-    userID: text("user_id").notNull(),
+    userId: text("user_id").notNull(),
     recipeId: integer("recipe_id").notNull(),
     title: text("title").notNull(),
     image: text("image"),
