d807a8884542f4a2ad81704730302bd7c578a31c
Gustavo Santiago <gustavohenriquesantiago5@gmail.com>
2026-09-07 11:34:28 -0300

    Enhance VerifyEmailScreen: implement email verification logic, improve UI with input fields and error handling

--- mobile/app/(auth)/verify-email.jsx
+++ mobile/app/(auth)/verify-email.jsx
@@ -1,12 +1,98 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Alert, KeyboardAvoidingView, Platform, ScrollView, Text, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { useSignUp } from '@clerk/expo';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { authStyles } from '@/assets/styles/auth.styles';
+import { Image } from 'expo-image';
+import { COLORS } from '@/constants/colors';
 
-const VerifyEmailScreen = () => {
+
+const VerifyEmailScreen = ({email, onBack}) => {
+  const {signUp, setActive} = useSignUp();
+  const [code, setCode] = useState("");
+  const [loading, setLoading] = useState(false); 
+
+  const handleVerification = async() =>{
+    if(!loading) return;
+    
+    setLoading(true)
+    try {
+      const signUpAttempt = await signUp.attemptEmailAddressVerification({code})
+
+      if(signUpAttempt.status === "complete"){
+        await setActive({session: signUpAttempt.createdSessionId})
+      } else {
+        Alert.alert("Error","Verification failed. Please try again.");
+        console.error(JSON.stringify(signUpAttempt, null, 2));
+      }
+
+    } catch (err) {
+      Alert.alert("Error",err.errors?.[0]?.message || "Verification failed")
+    }finally{
+        setLoading(false)
+    }
+  }
+
   return (
-    <View>
-      <Text>VerifyEmailScreen</Text>
-    </View>
-  )
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={authStyles.container}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
+          style={authStyles.keyboardView}
+        >
+          <ScrollView contentContainerStyle={authStyles.scrollContent} showsVerticalScrollIndicator={false}>
+
+            {/* Image Container */}
+              <View style={authStyles.imageContainer}>
+                <Image
+                  source={require("../../assets/images/i3.png")}
+                  style={authStyles.image}
+                  contentFit='contain'
+                />
+              </View>
+
+              {/* Title */}
+                <Text style={authStyles.title}>Verify Your Email </Text>
+                <Text style={authStyles.subtitle}>We&apos;ve sent a verification code to{email} </Text>
+
+              <View style={authStyles.formContainer}>
+                {/* Verification Code input */}
+                <View style={authStyles.inputContainer}>
+                  <TextInput
+                    style={authStyles.textInput}
+                    placeholder="Enter verification code"
+                    placeholderTextColor={COLORS.textLight}
+                    value={code}
+                    onChangeText={setCode}
+                    keyboardType='number-pad'
+                    autoCapitalize='none'
+                  />
+                </View>
+
+                {/* Verify Button */}
+                <TouchableOpacity
+                  style={[authStyles.authButton, loading && authStyles.buttonDisabled]}
+                  onPress={handleVerification}
+                  disabled={loading}
+                  activeOpacity={0.8}
+                >
+                  <Text style={authStyles.buttonText}>{loading ? "Verify..." : "Verify Email"}</Text>
+                </TouchableOpacity>
+
+                {/* Back to Sign Up */}
+                <TouchableOpacity style={authStyles.linkContainer} onPress={onBack} >
+                  <Text style={authStyles.linkText}> <Text style={authStyles.link}>Back to sign Up</Text> </Text>
+
+                </TouchableOpacity>
+              </View>
+
+          </ScrollView>
+
+        </KeyboardAvoidingView>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 export default VerifyEmailScreen
